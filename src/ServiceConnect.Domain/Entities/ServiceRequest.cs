b9b25c4d96dc0ca596bd5140dab4fa1bf5632c18
João Lucas <j.lucasffilho@gmail.com>
2025-05-12 20:53:32 -0300

    feat: add `ServiceRequest` entity and `ServiceStatus` enum

---
+++ src/ServiceConnect.Domain/Entities/ServiceRequest.cs
@@ -0,0 +1,14 @@
+﻿using ServiceConnect.Domain.Enums;
+
+namespace ServiceConnect.Domain.Entities;
+
+public class ServiceRequest
+{
+    public Guid Id { get; set; }
+    public Guid ServiceId { get; set; }
+    public Guid CustomerId { get; set; }
+    public ServiceStatus Status { get; set; }
+    public DateTime RequestedAt { get; set; }
+    public DateTime? AccepetedAt { get; set; }
+    public DateTime? CompletedAt { get; set; }
+}
---
+++ src/ServiceConnect.Domain/Enums/ServiceStatus.cs
@@ -0,0 +1,7 @@
+﻿namespace ServiceConnect.Domain.Enums;
+public enum ServiceStatus
+{
+    Pending = 1,
+    Accepted = 2,
+    Completed = 3
+}
