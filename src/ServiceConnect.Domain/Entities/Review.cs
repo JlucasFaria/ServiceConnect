c5603822bf0d6ee9cc1c98b682e35cd534547afe
João Lucas <j.lucasffilho@gmail.com>
2025-05-19 23:18:30 -0300

    feat: create Review entity

---
+++ src/ServiceConnect.Domain/Entities/Review.cs
@@ -0,0 +1,10 @@
+﻿namespace ServiceConnect.Domain.Entities;
+public class Review
+{
+    public Guid Id { get; set; }
+    public Guid ServiceId { get; set; }
+    public Guid CustomerId { get; set; }
+    public int Rating { get; set; }
+    public string Comment { get; set; } = string.Empty;
+    public DateTime CreatedAt { get; set; }
+}
